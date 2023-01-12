40a63b64b2c25405b05c1e15b69ade3e095a6f3d
Aggelos Margkas <juniormarg99@gmail.com>
2023-01-13 00:26:19 +0200

    Full Dependency Graph

---
+++ dep_graph.py
@@ -0,0 +1,55 @@
+import json
+from collections import deque
+
+INITIAL_PATH = '../tmp/deps.json'
+
+
+def read_json_file(filepath):
+    with open(filepath, 'r') as f:
+        return json.load(f)
+
+
+def create_dependency_graph(dependencies):
+    graph = {}
+    for pkg, deps in dependencies.items():
+        graph[pkg] = deps
+        for dep in deps:
+            if dep not in graph:
+                graph[dep] = []
+    return "Input Graph.. " + str(graph)
+
+
+def dep_graph(filename):
+
+    output = []
+    dependencies = read_json_file(filename)
+    graph = create_dependency_graph(dependencies)
+    print(graph)
+    print("Output graph ..")
+    print("\n")
+
+    return temp(dependencies,  output)
+
+
+def temp(graph, dependency):
+
+    for node in graph:
+        queue = deque()
+        queue.append(node)
+
+        while queue:
+            s = queue.pop()
+            dependency.append(s)
+
+            for x in graph[s][::-1]:
+                if x == node:
+                    raise Exception("Ups, a circle appeared in our dependencies! Sorry, I will never terminate, "
+                                    "need to go.")
+                else:
+                    queue.append(x)
+
+    print(dependency)
+    return dependency
+
+
+dep_graph(INITIAL_PATH)
---
+++ test_dep_graph.py
@@ -0,0 +1,35 @@
+from dep_graph.dep_graph import dep_graph
+import pytest
+import os
+
+folder_path = './tmp'
+list_of_tests = []
+
+for root, dirs, files in os.walk(folder_path):
+    for file in files:
+        file_path = os.path.join(root, file)
+        list_of_tests.append(file_path)
+
+
+class TestClass:
+
+    def test_initial(self):
+        path = list_of_tests[0]
+        assert dep_graph(path) == ['pkg1', 'pkg2', 'pkg3', 'pkg3', 'pkg2', 'pkg3', 'pkg3']
+
+    def test_scale(self):
+        path = list_of_tests[1]
+        assert dep_graph(path) == ["pkg1", "pkg2", "pkg3", "pkg4", "pkg4", "pkg3", "pkg4", "pkg2", "pkg3", "pkg4", "pkg4", "pkg3", "pkg4", "pkg4",]
+
+    def test_circle(self):
+        path = './tmp/deps2.json'
+        with pytest.raises(Exception, match="Ups") as e_info:
+            dep_graph(path)
+            print(e_info)
+
+    def test_empty(self):
+        path = './tmp/deps3.json'
+        assert dep_graph(path) == ['pkg1', 'pkg2', 'pkg3']
+
+
+
